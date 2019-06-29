fdbfcd65b99fd7ae6f2288389670a04ce1712cf2
Nate Foss <npfoss@gmail.com>
2019-06-29 15:26:49 -0400

    add file loader

--- src/index.js
+++ src/index.js
@@ -2,10 +2,37 @@
 
 const IPFS = require('ipfs')
 
+/// UTILS
+const FILE_TYPES = {
+  FILE: 0,
+  DIRECTORY: 1,
+};
+// source: https://github.com/ipfs/js-ipfs/blob/master/examples/browser-mfs/filetree.js
+const loadFiles = async function (ipfs, path) {
+  const output = {};
+  path = path.replace(/\/\/+/g, '/');
 
+  const contents = await ipfs.files.ls(path, {
+    long: true,
+  })
+    .catch(error => console.error(error));
+
+  for (let i = 0; i < contents.length; i++) {
+    const entry = contents[i];
+    output[entry.name] = entry;
+
+    if (entry.type === FILE_TYPES.DIRECTORY) {
+      entry.contents = await loadFiles(ipfs, `${path}/${entry.name}`);
+    }
+  }
+
+  return output;
+};
+
+
+/// the protocol
 class GravityProtocol {
 	constructor () {
-
 		const node = new IPFS()
 
 		this.ready = false;
@@ -17,7 +44,12 @@ class GravityProtocol {
 			this.ready = true;
 		})
 
-		
+		this.loadFiles = async function (path) {
+			if (!this.ready){
+				throw new Error("IPFS node isn't ready yet");
+			}
+			return loadFiles(node, path)
+		}
 
 	}
 }
