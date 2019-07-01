54e9b9b85b690ced9ce1ff70bbcd9056d351a9d6
Nate Foss <npfoss@gmail.com>
2019-07-01 00:34:25 -0400

    add linting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-'use strict'
 
-const IPFS = require('ipfs')
+const IPFS = require('ipfs');
 
-/// UTILS
+//*  UTILS
 const FILE_TYPES = {
   FILE: 0,
   DIRECTORY: 1,
@@ -10,19 +9,19 @@ const FILE_TYPES = {
 // source: https://github.com/ipfs/js-ipfs/blob/master/examples/browser-mfs/filetree.js
 const loadDirs = async function (ipfs, path) {
   const output = {};
-  path = path.replace(/\/\/+/g, '/');
+  const cleanpath = path.replace(/\/\/+/g, '/');
 
-  const contents = await ipfs.files.ls(path, {
+  const contents = await ipfs.files.ls(cleanpath, {
     long: true,
   })
     .catch(error => console.error(error));
 
-  for (let i = 0; i < contents.length; i++) {
+  for (let i = 0; i < contents.length; i += 1) {
     const entry = contents[i];
     output[entry.name] = entry;
 
     if (entry.type === FILE_TYPES.DIRECTORY) {
-      entry.contents = await loadDirs(ipfs, `${path}/${entry.name}`);
+      entry.contents = await loadDirs(ipfs, `${cleanpath}/${entry.name}`);
     }
   }
 
@@ -30,30 +29,28 @@ const loadDirs = async function (ipfs, path) {
 };
 
 
-/// the protocol
+//*  the protocol
 class GravityProtocol {
-	constructor () {
-		const node = new IPFS()
+  constructor() {
+    const node = new IPFS();
 
-		this.ready = false;
+    this.ready = false;
 
-		node.on('ready', () => {
-			// Ready to use!
-			// See https://github.com/ipfs/js-ipfs#core-api
+    node.on('ready', () => {
+      // Ready to use!
+      // See https://github.com/ipfs/js-ipfs#core-api
 
-			this.ready = true;
-		})
+      this.ready = true;
+    });
 
-		this.loadDirs = async function (path) {
-			if (!this.ready){
-				throw new Error("IPFS node isn't ready yet");
-			}
-			return loadDirs(node, path)
-		}
-
-	}
+    this.loadDirs = async function (path) {
+      if (!this.ready) {
+        throw new Error("IPFS node isn't ready yet");
+      }
+      return loadDirs(node, path);
+    };
+  }
 }
 
 
-
-module.exports = GravityProtocol
+module.exports = GravityProtocol;
