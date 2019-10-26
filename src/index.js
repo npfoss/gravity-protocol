ede2aa035a53db06ff311d0f60a821bb71e58498
Nate Foss <npfoss@gmail.com>
2019-10-26 17:10:26 -0400

    add www to magic link

--- src/index.js
+++ src/index.js
@@ -1213,7 +1213,7 @@ class GravityProtocol extends EventEmitter {
       if (publicKey !== undefined) obj.publicKey = publicKey;
       if (addresses !== undefined) obj.addresses = addresses;
 
-      return 'https://gravitynet.io/magic/'.concat(sodium.to_base64(JSON.stringify(obj)));
+      return 'https://www.gravitynet.io/magic/'.concat(sodium.to_base64(JSON.stringify(obj)));
     };
 
     // this is what you share to get people to add you
