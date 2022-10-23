9908bd3b11fd30618e5139dc6824d1c6231d01c0
Nate Foss <npfoss@gmail.com>
2022-10-22 22:11:36 -0400

    start over with TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,34 @@
 module.exports = {
-	"extends": "airbnb-base",
-	"rules": {
-		"func-names": ["warn", "as-needed"],
-	},
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  ignorePatterns: ["**/node_modules/**", "**/dist/**", "tsconfig.json"],
+  rules: {
+    semi: "error",
+    indent: ["error", 2, { SwitchCase: 1 }],
+
+    // "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-function": "off",
+
+    eqeqeq: ["error", "smart"],
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/consistent-type-assertions": [
+      "error",
+      {
+        assertionStyle: "never",
+      },
+    ],
+
+    "no-undef": "off",
+  },
 };
