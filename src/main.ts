6fde7113e55fb713a24da468f1e824755da258b1
Nate Foss <npfoss@gmail.com>
2023-02-20 13:43:14 -0500

    yarn with node_modules, and add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,4 @@
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
-const sleep = (ms:number) => new Promise((res) => setTimeout(res, ms))
+console.log("1");
+sleep(100).then(() => console.log(2));
